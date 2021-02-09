fb7f824432971e1b2fba88bf836f9218d078f085
denis <mrisholigare@gmail.com>
2021-02-09 23:06:57 +0300

    environment imported @data.service.ts

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,5 +1,7 @@
+
 import { Injectable } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import {environment} from '../environments/environment';
 @Injectable({
   providedIn: 'root'
 })
